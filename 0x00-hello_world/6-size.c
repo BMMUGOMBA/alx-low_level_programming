26b1792d10d49d20742806432fe181970946287f
BMMUGOMBA <bmmugomba@gmail.com>
2023-02-16 15:54:53 +0200

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	float floatType;
 	/*
 	 * size of evaluates size of variable
-	 * /
+	 */
 	 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	 printf("Size of a long int: %zu byte(s)\n", sizeof(lintType));
