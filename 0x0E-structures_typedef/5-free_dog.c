1d3457ab878c65138120c8d5223e3532195ab24b
BMMUGOMBA <bmmugomba@gmail.com>
2023-03-21 02:13:09 +0200

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,18 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
 
 /**
- * free_dog - frees a dog structure
- * @d: pointer to struct to free
- * Return: Nothing
- */
-
+  * free_dog - ...
+  * @d: ...
+  *
+  * Return: ...
+  */
 void free_dog(dog_t *d)
 {
-	if (d)
+	if (d != NULL)
 	{
-		free(d->name);
 		free(d->owner);
+		free(d->name);
 		free(d);
 	}
 }
