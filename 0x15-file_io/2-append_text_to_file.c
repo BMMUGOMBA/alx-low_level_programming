3bf0340f55190ca5cec154b114ef08fabd83d905
BMMUGOMBA <bmmugomba@gmail.com>
2023-04-12 16:38:59 +0200

    2 if else edit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,19 +9,18 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, len, w;
-    int fStatus = -1;
 
 	if (filename == NULL)
-		return (fStatus);
+		return (-1);
 	fd = open(filename, O_APPEND | O_WRONLY);
-	if (fd == fStatus)
+	if (fd < 0)
 		return (-1);
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len] != '\0'; len++)
 		;
 		w = write(fd, text_content, len);
-		if (w == fStatus)
+		if (w < 0)
 			return (-1);
 	}
 	close(fd);
