7afa7be13c86b5be437b3be08898e64ae7710998
BMMUGOMBA <bmmugomba@gmail.com>
2023-04-12 16:37:22 +0200

    simple edit 2 -1

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		;
 		w = write(fd, text_content, len);
 		if (w == fStatus)
-			return (fStatus);
+			return (-1);
 	}
 	close(fd);
 	return (1);
