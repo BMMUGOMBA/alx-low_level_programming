9f52a5bfae3b558055c8f9d0a3b00a703377cf0a
BMMUGOMBA <bmmugomba@gmail.com>
2023-04-12 13:57:17 +0200

    question 0 file and mian read text

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *O/file_write - the actual number of bytes the function can read and print.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	 ssize_t file_open, file_read, file_write;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer ==  NULL)
+		return (0);
+
+	file_open = open(filename, O_RDONLY);
+	file_read = read(file_open, buffer, letters);
+	file_write = write(STDOUT_FILENO, buffer, file_read);
+
+	if (file_open == -1 || file_read == -1 || file_write == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(file_open);
+
+	return (file_write);
+
+}
