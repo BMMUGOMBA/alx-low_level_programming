e27b0f69c7a2c0881d2f185ef394447809c70f0a
BMMUGOMBA <bmmugomba@gmail.com>
2023-04-12 16:35:50 +0200

    2 corrections

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,19 +9,20 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, len, w;
+    int fStatus = -1;
 
 	if (filename == NULL)
-		return (-1);
+		return (fStatus);
 	fd = open(filename, O_APPEND | O_WRONLY);
-	if (fd == -1)
+	if (fd == fStatus)
 		return (-1);
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len] != '\0'; len++)
 		;
 		w = write(fd, text_content, len);
-		if (w == -1)
-			return (-1);
+		if (w == fStatus)
+			return (fStatus);
 	}
 	close(fd);
 	return (1);
