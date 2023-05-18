58dc3cf797ccdaf336f75a3be8d8278052fdc8f3
BMMUGOMBA <bmmugomba@gmail.com>
2023-05-18 16:39:50 +0200

    file 7 to advanced

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,51 +1,54 @@
 #include "lists.h"
-/**
-  * *insert_dnodeint_at_idx - Insert a new node into a doubly
-  * linked list at a given index
-  * @head: Double pointer to the start of the list
-  * @idx: Index to insert new node at
-  * @n: Value to assign to new node
-  * Return: Address of the new node, NULL if it fails
-  */
-dlistint_t *insert_dnodeint_at_idx(dlistint_t **head, unsigned int idx, int n)
-{
-	unsigned int pos;
-	dlistint_t *new_node, *current, *prev;
 
-	current = *head;
-	if (*head == NULL && idx != 0)
-		return (NULL);
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	if (*head != NULL)
+/**
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
+ *
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
+
+	new = NULL;
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
 	{
-		prev = NULL;
-		while (current->prev != NULL)
-			current = current->prev;
-		for (pos = 0; current != NULL && pos < idx; pos++)
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
 		{
-			prev = current;
-			current = current->next;
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
 		}
-		if (pos == idx)
-		{
-			new_node->n = n;
-			new_node->prev = prev;
-			if (current != NULL)
-				current->prev = new_node;
-			new_node->next = current;
-			if (idx != 0)
-				prev->next = new_node;
-			else
-				*head = new_node;
-			return (new_node);
-		}
-		return (NULL);
 	}
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	new_node->n = n;
-	*head = new_node;
-	return (new_node);
+
+	return (new);
 }
